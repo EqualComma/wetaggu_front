b65aadfc6c21c7c16422bc065887ba2a9a6c299d
jeongsehyun <pacemaker.for.us@gmail.com>
2024-08-11 22:02:48 +0900

    공통컴포넌트 초기 작업

---
+++ src/components/common/texts/Text/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Text.module.scss";
+
+const Text = ({ children, as: Component = "p", className = "", size = "medium", color = "default" }) => {
+  return <Component className={`${styles.text} ${styles[size]} ${styles[color]} ${className}`}>{children}</Component>;
+};
+
+export default Text;
